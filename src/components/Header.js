e78a1aea2efe76698b97443db034e81698416bef
SamuelHallman <92924123+SamuelHallman@users.noreply.github.com>
2023-02-24 08:59:32 +0200

    Add files via upload
    You have to create your own React app to run this code in 'src' folder. Just replace all the files in 'src' and it should work.

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = ({ handleToggleDarkMode}) => {
+    return(
+        <div className="header">
+            <h1>Keep Notes</h1>
+            <button onClick={() => 
+                handleToggleDarkMode(
+                    (previousDarkMode)=> !previousDarkMode
+                    )
+                } 
+                className="save"
+            >
+                Toggle Dark Mode
+            </button>
+        </div>
+    )
+}
+
+export default Header;
